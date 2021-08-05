ee705df793cb807fbecc1d4d1624d49e7e8f03b4
Sergey <piolersa85@mail.ru>
2021-08-05 02:31:04 +0200

    formatter

--- Шаги и Цели 2/ViewController.swift
+++ Шаги и Цели 2/ViewController.swift
@@ -32,6 +32,32 @@ class ViewController: UIViewController {
         vc.title = "New Reminder"
         vc.navigationItem.largeTitleDisplayMode = .never
         vc.completion = { title, body, date in
+            DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                let new = MyReminder(title: title, date: date, identifier: "id_ \(title)")
+                self.models.append(new)
+                self.table.reloadData()
+                
+                let content = UNMutableNotificationContent()
+                content.title  = title
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date //Date().addingTimeInterval(10)
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents( [.year, .month, .day, .hour, .minute, . second],
+                                                                                                          from: targetDate)  ,
+                                                            
+                                                            repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                    
+                    })
+                
+                }
             
         }
         
@@ -113,7 +139,10 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = models[indexPath.row].title
-        
+        let date = models[indexPath.row].date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM, dd, YYYY"
+        cell.detailTextLabel?.text = formatter.string(from: date)
         
         
         
